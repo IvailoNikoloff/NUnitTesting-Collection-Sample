d42a39ce4962dd6cd877ba6cef0ad9d1628c57a6
IvailoNikoloff <ivailo.nikoloff@gmail.com>
2023-01-23 21:50:25 +0200

    initial commit

---
+++ Collection.UnitTests/CollectionTests.cs
@@ -0,0 +1,15 @@
+namespace Collection.UnitTests;
+
+public class CollectionTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
